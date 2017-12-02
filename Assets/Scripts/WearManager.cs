95c5da8b1550df4574128325e039b2412520b575
C.Rondeau <rondeau.clement@gmail.com>
2017-12-02 20:08:13 +0100

    Working on new wear system

--- Assets/Scripts/WearManager.cs
+++ Assets/Scripts/WearManager.cs
@@ -10,10 +10,12 @@ public class WearManager : MonoBehaviour {
         public int health;
     }
 
-    public float scaleFactor = 1.5f;
-    public float repairValue = 25f;
+    public float scaleFactor = 1f;
+    public SpriteRenderer[] walls = new SpriteRenderer[4];
+    public Sprite[] wallSprite = new Sprite[6];
 
-    private Wall[] walls = new Wall[4];
+    private Wall[] _walls = new Wall[4];
+    private float _timeBeforeDecrease;
 
     private void Start()
     {
@@ -22,31 +24,41 @@ public class WearManager : MonoBehaviour {
         // Create logical walls
         for (int i = 0; i < 4; i++)
         {
-            walls[i] = new Wall();
-            walls[i].health = 6;
+            _walls[i] = new Wall();
+            _walls[i].health = 6;
         }
+
+        _timeBeforeDecrease = (ResourceManager.maxBunnies / (float) ResourceManager.GetNumberOfBunnies()) * scaleFactor;
+        
+        StartCoroutine(LoseWear());
     }
 
-    private void Update ()
+    IEnumerator LoseWear()
     {
+        yield return new WaitForSeconds(_timeBeforeDecrease);
 
+        // Find wall with the biggest health
+        int maxHealth = 0;
+        List<int> walls = new List<int>();
+        int index = 0;
+        foreach (var wall in _walls)
+        {
+            if(wall.health >= maxHealth)
+            {
+                maxHealth = wall.health;
+                walls.Add(index);
+            }
+            index++;
+        }
 
-        // Increase the wear rate when we've more bunnies
-        //_increasePerSecond = (ResourceManager.GetNumberOfBunnies() / (float) ResourceManager.maxBunnies) * scaleFactor;
-        //_increaseRate = _increasePerSecond / 60f;
+        // Get random wall that will get damaged
+        int wallIndex = walls[Random.Range(0, walls.Count)];
+        // Change its sprite according to its health
+        this.walls[wallIndex].sprite = wallSprite[_walls[wallIndex].health];
 
-        //_wear += _increaseRate;
-
-        //print(_wear);
-        
-        // If the wear is over the maximum wear value we lose
-        //if(_wear >= _maxWear && Time.timeScale > 0)
-        //{
-        //    EventManager.TriggerEvent("LoseWear");
-        //}
-
-
-	}
+        _timeBeforeDecrease = (ResourceManager.maxBunnies / (float)ResourceManager.GetNumberOfBunnies()) * scaleFactor;
+        StartCoroutine(LoseWear());
+    }
 
     private void RepairShip()
     {
