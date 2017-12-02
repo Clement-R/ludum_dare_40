d8ddb7d51a1565f27b2d125289fa32ea2a5135e8
C.Rondeau <rondeau.clement@gmail.com>
2017-12-02 13:19:20 +0100

    Add player movement and some walls to test collisions

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float _minSpeed = 120f;
+    [SerializeField]
+    private float _maxSpeed = 1000f;
+    [SerializeField]
+    private float smoothTime = 0.15f;
+    private Vector2 velocity = Vector2.zero;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2.SmoothDamp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), ref velocity, smoothTime, _maxSpeed, Time.deltaTime);
+        _rb.velocity = velocity;
+    }
+}
