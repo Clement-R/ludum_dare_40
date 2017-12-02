fe8b4c8e8cf21d366fcb8f151a7c96f145623aef
C.Rondeau <rondeau.clement@gmail.com>
2017-12-02 19:31:47 +0100

    Working on last interactive zone : repair zone

---
+++ Assets/Scripts/MotorBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pkm.EventManager;
+
+public class MotorBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private int _maxSpeed = 600;
+    [SerializeField]
+    private int _initialSpeed = 600;
+    [SerializeField]
+    private int _decreasePerSecond = 5;
+    [SerializeField]
+    private int _bunnyPower = 25;
+
+    private float _speed;
+    private float _decreaseRate;
+
+    private void Start ()
+    {
+        EventManager.StartListening("UseBunnyInMotor", AddPower);
+        _speed = _initialSpeed;
+        _decreaseRate = _decreasePerSecond / 60f;
+    }
+	
+    private void AddPower()
+    {
+        _speed += _bunnyPower;
+        _speed = Mathf.Clamp(_speed, 0, _maxSpeed);
+    }
+
+	private void Update ()
+    {
+        _speed -= _decreaseRate;
+	}
+}
---
+++ Assets/Scripts/WearManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pkm.EventManager;
+
+public class WearManager : MonoBehaviour {
+
+    struct Wall
+    {
+        public int health;
+    }
+
+    public float scaleFactor = 1.5f;
+    public float repairValue = 25f;
+
+    private Wall[] walls = new Wall[4];
+
+    private void Start()
+    {
+        EventManager.StartListening("UseRepairZone", RepairShip);
+
+        // Create logical walls
+        for (int i = 0; i < 4; i++)
+        {
+            walls[i] = new Wall();
+            walls[i].health = 6;
+        }
+    }
+
+    private void Update ()
+    {
+
+
+        // Increase the wear rate when we've more bunnies
+        //_increasePerSecond = (ResourceManager.GetNumberOfBunnies() / (float) ResourceManager.maxBunnies) * scaleFactor;
+        //_increaseRate = _increasePerSecond / 60f;
+
+        //_wear += _increaseRate;
+
+        //print(_wear);
+        
+        // If the wear is over the maximum wear value we lose
+        //if(_wear >= _maxWear && Time.timeScale > 0)
+        //{
+        //    EventManager.TriggerEvent("LoseWear");
+        //}
+
+
+	}
+
+    private void RepairShip()
+    {
+        //_wear -= repairValue;
+        //_wear = Mathf.Clamp(_wear, 0f, _maxWear);
+    }
+}
