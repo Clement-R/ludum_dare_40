fbc66ca7d53cd6a3124c53bebb61940a73f92acf
C.Rondeau <rondeau.clement@gmail.com>
2017-12-03 18:20:48 +0100

    Add sounds on menu

---
+++ Assets/ButtonBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonBehavior : MonoBehaviour, IPointerEnterHandler {
+
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        AkSoundEngine.PostEvent("Play_select", gameObject);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AkSoundEngine.PostEvent("Play_overbutton", gameObject);
+    }
+}
